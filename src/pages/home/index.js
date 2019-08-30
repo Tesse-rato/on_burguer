94fb79a7fbcb87f60f776f71ce2d807f3b0f4fc3
Bruno França <xurask8@gmail.com>
2019-08-30 18:38:50 -0300

    Organizando aos poucos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,82 +2,32 @@ import React, { Component } from 'react';
 import './home.css';
 
 import AnimatedLogo from '../../components/AnimatedLogo';
-
 import Header from '../../components/header';
-import ImageHeader from '../../assets/burguer-header.jpg';
-import ImageHeader2 from '../../assets/burguer-header2.jpg';
-import ImageHeader3 from '../../assets/burguer-header3.jpg';
-import ImageHeader4 from '../../assets/burguer-header4.jpg';
+import SliderHeader from '../../components/SliderHeader';
+import SocialMedia from '../../components/SocialMedia';
 
 export default class Home extends Component {
-  /**
-    componentDidMount() {
-      console.log("Component vai montar ");
-  
-      this.burguer = document.getElementById("Uai");
-  
-      this.burguer.addEventListener('click', () => {
-        this.burguer.classList.add('current');
-        console.log("Functionu em")
-      });
-  
-    }
-*/
 
-
-  componentDidMount() {
-    const img1 = document.getElementById("img-burguer");
-    const img2 = document.getElementById("img-burguer2");
-    const img3 = document.getElementById("img-burguer3");
-    const img4 = document.getElementById("img-burguer4");
-
-    setTimeout(() => {
-      img1.classList.add('desactive');
-      img2.classList.add('active');
-      setTimeout(() => {
-        img1.classList.remove('desactive');
-        img1.classList.remove('active');
-        img2.classList.add('desactive');
-        img3.classList.add('active');
-        setTimeout(() => {
-          img2.classList.remove('desactive');
-          img2.classList.remove('active');
-          img3.classList.add('desactive');
-          img4.classList.add('active');
-          setTimeout(() => {
-            img3.classList.remove('desactive');
-            img3.classList.remove('active');
-            img4.classList.add('desactive');
-            img1.classList.add('active');
-            this.componentDidMount();
-            setTimeout(() => {
-              img4.classList.remove('desactive');
-              img4.classList.remove('active');
-            }, 5000);
-          }, 5000);
-        }, 5000);
-      }, 5000);
-    }, 8000);
-
-  }
   render() {
     return (
       <div className="Container" >
-        {/*
-      */}
-
-        <div className="Logo-Container" >
-          <AnimatedLogo />
-        </div>
 
         <Header />
 
-        <div className="Slide-Container">
-          <img alt="Nao foi possivel carregar" id="img-burguer" src={ImageHeader} className="Burguer active" />
-          <img alt="Nao foi possivel carregar" id="img-burguer2" src={ImageHeader2} className="Burguer" />
-          <img alt="Nao foi possivel carregar" id="img-burguer3" src={ImageHeader3} className="Burguer" />
-          <img alt="Nao foi possivel carregar" id="img-burguer4" src={ImageHeader4} className="Burguer" />
-        </div>
+        <SliderHeader >
+          <div className="Logo-Container" >
+            <AnimatedLogo />
+          </div>
+          <div className="Description-Container" >
+            <span className="Title" >Tupaciguara - MG</span>
+            <span className="Sub-Title" >Rua Bueno Brandão, 143.</span>
+            <span className="Sub-Title" >Em frente à praça do Estadual</span>
+            <span className="Sub-Title" >Aberto das 19:00h ás 00:00h</span>
+            <span className="Title below">Contatos</span>
+            <span className="Sub-Title" style={{ fontSize: '2.5vw', fontFamily: 'Sans' }}>WhatsApp (34) 99181-5364</span>
+            <SocialMedia />
+          </div>
+        </SliderHeader>
       </div>
     );
   }
